de0f62fdc8e9c25acd18567e09b4797adf742b48
moPc <mozhongwen93@foxmail.com>
2020-05-02 20:17:09 +0800

    2020.5.2 --2

--- smallsteps_back.js
+++ smallsteps_back.js
@@ -31,7 +31,7 @@ app.get("/test", function (req, res) {
   res.header("Access-Control-Allow-Origin", "*"); //允许全网跨域访问
   //这是一个回调函数
   var sql = "select * from daytargetlist;";
-  getInfo(sql, function (result) {
+  setMysql(sql, function (result) {
     res.send(result);
   });
 });
@@ -39,54 +39,74 @@ app.get("/test", function (req, res) {
 //上传目标(没有插入，有更新)
 app.post("/setTarget", function (req, res) {
   console.log(req.body);
-  if (req.body.name === 123) {
-    let sql =
-      "select * from daytargetlist where timestamp = " +
-      req.body.timestamp +
-      " and userId = " +
-      req.body.userId;
-    getInfo(sql, function (result) {
-      if (result.length != 0) {
-        console.log("更新");
-        sql =
-          "UPDATE daytargetlist SET target='" +
-          req.body.target +
-          "' where timestamp = " +
-          req.body.timestamp +
-          " and userId = " +
-          req.body.userId;
-      } else {
-        console.log("插入");
-        sql =
-          "INSERT INTO daytargetlist (userId,timestamp,target) VALUES (" +
-          req.body.userId +
-          "," +
-          req.body.timestamp +
-          ",'" +
-          req.body.target +
-          "')";
-      }
-      getInfo(sql, function (result) {
-        res.send(result);
-      });
+  let sql =
+    "select * from daytargetlist where timestamp = " +
+    req.body.timestamp +
+    " and userId = " +
+    req.body.userId;
+  setMysql(sql, function (result) {
+    if (result.length != 0) {
+      console.log("更新");
+      sql =
+        "UPDATE daytargetlist SET target='" +
+        req.body.target +
+        "' where timestamp = " +
+        req.body.timestamp +
+        " and userId = " +
+        req.body.userId;
+    } else {
+      console.log("插入");
+      sql =
+        "INSERT INTO daytargetlist (userId,timestamp,target) VALUES (" +
+        req.body.userId +
+        "," +
+        req.body.timestamp +
+        ",'" +
+        req.body.target +
+        "')";
+    }
+    setMysql(sql, function (result) {
+      let respone = {
+        title: "success",
+      };
+      res.send(respone);
     });
-  }
+  });
 });
 
-//查询
-
-app.post("/find", function (req, res) {
+//获取今日目标
+app.post("/getTodayTarget", function (req, res) {
   console.log(req.body);
-  let sql = "select * from daytargetlist where timestamp = 333 and userId = 1";
-  getInfo(sql, function (result) {});
+  let sql =
+    "select * from daytargetlist where timestamp = " +
+    req.body.timestamp +
+    " and userId = " +
+    req.body.userId;
+  setMysql(sql, function (result) {
+    res.send(result);
+  });
+});
+
+//时间查询
+app.post("/timeTarget", function (req, res) {
+  let sql =
+    "select * from daytargetlist where timestamp BETWEEN " +
+    req.body.startTime +
+    " and " +
+    req.body.endTime +
+    " and userId = " +
+    req.body.userId +
+    " ;";
+  setMysql(sql, function (result) {
+    res.send(result);
+  });
 });
 
 //监听端口
 var server = app.listen(7001, function () {});
 
 //mysql封装函数
-//查询
-function getInfo(sql, next) {
+function setMysql(sql, next) {
   //连接数据库
   var connection = mysql.createConnection({
     host: "localhost", //数据库域名，localhost表示同域名，也就是同一台服务器
@@ -96,40 +116,16 @@ function getInfo(sql, next) {
     database: "cloundlisten", //调用的数据库中具体的库
   });
   connection.connect(); //链接数据库
-  //获取当前库中表的名称
+
   connection.query(sql, function (err, result) {
     if (err) {
       console.log(err);
       console.log("error!");
-      next("error!");
+      next("error");
     } else {
       console.log("获取数据库返回数据");
       console.log(result);
       next(result);
-    }
-  });
-}
-
-//插值
-function setInfo(sql, sql2, next) {
-  //连接数据库
-  var connection = mysql.createConnection({
-    host: "localhost", //数据库域名，localhost表示同域名，也就是同一台服务器
-    user: "root", //数据库用户名
-    password: "123456", //数据库密码
-    port: "3306", //数据库域名监听的端口
-    database: "cloundlisten", //调用的数据库中具体的库
-  });
-  connection.connect(); //链接数据库
-
-  connection.query(sql, sql2, function (err, result) {
-    if (err) {
-      console.log(err);
-      console.log("error!");
-      next("error!");
-    } else {
-      console.log(result);
-      next();
     }
   });
 }
